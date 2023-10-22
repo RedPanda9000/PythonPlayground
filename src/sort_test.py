1dfa2d8443dca2899806d42813acea5b0fcebf6e
projects@mkdirdev.co.uk <projects@mkdirdev.co.uk>
2023-10-22 18:52:41 +0100

    feature/PageObjectModel - Added Started on Navigation - Added Placed in ReadMe.md - Added Placed in requirements.txt to list dependencies - Updated altered sort test to use assertEqual as opposed to assertEquals (deprecated) - Added firefox and chrome test running - Added file upload on pipeline

--- src/sort_test.py
+++ src/sort_test.py
@@ -8,7 +8,7 @@ class TestDinosaurs(unittest.TestCase):
     def test_herbyShouldComeBack(self):
         unit = Dinosaurs(["raptor", "t-rex"])
         results = unit.sort(["triceratops","raptor", "t-rex","brachiosaurus", "lemons", "stegosaurus", "allosaurus"])
-        self.assertEquals(results,["triceratops", "brachiosaurus", "lemons", "stegosaurus", "allosaurus"])
+        self.assertEqual(results,["triceratops", "brachiosaurus", "lemons", "stegosaurus", "allosaurus"])
         
 if __name__=='__main__':
     unittest.main()
